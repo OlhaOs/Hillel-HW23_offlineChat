47a3bee1d3443fbb934d2dc9ce5dd8751ceb6789
OlhaOs <olalaadka@gmail.com>
2023-06-03 16:24:45 +0300

    after review

--- src/App.js
+++ src/App.js
@@ -2,36 +2,25 @@ import './App.css';
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { DateTime } from 'luxon';
-import LuckSkywalker from './Smiles/LuckSkywalker.png';
-import DarthVader from './Smiles/DarthVader.png';
-import ObiWanKenobi from './Smiles/ObiWanKenobi.png';
-import R2D2 from './Smiles/R2D2.png';
-import C3PO from './Smiles/C3PO.png';
 
 function App() {
   const [message, setMessage] = useState('');
   const [sentMsg, setSentMsg] = useState([]);
   const [nickName, setNickName] = useState('');
 
-  const nickNames = [
-    'Luck Skywalker',
-    'Dart Vader',
-    'Obi-Wan Kenobi',
-    'R2-D2',
-    'C-3PO',
+  const userProfile = [
+    {
+      nickName: 'Luck Skywalker',
+      avatar: '/avatars/LuckSkywalker.png',
+    },
+    { nickName: 'Dart Vader', avatar: '/avatars/DarthVader.png' },
+    { nickName: 'Obi-Wan Kenobi', avatar: '/avatars/ObiWanKenobi.png' },
+    { nickName: 'R2-D2', avatar: '/avatars/R2D2.png' },
+    { nickName: 'C-3PO', avatar: '/avatars/C3PO.png' },
   ];
-
-  const avatars = {
-    'Luck Skywalker': LuckSkywalker,
-    'Dart Vader': DarthVader,
-    'Obi-Wan Kenobi': ObiWanKenobi,
-    'R2-D2': R2D2,
-    'C-3PO': C3PO,
-  };
 
   const handleSubmit = e => {
     e.preventDefault();
-
     const currentTime = DateTime.local().toFormat('HH:mm:ss');
     const newMsg = {
       message,
@@ -51,33 +40,9 @@ function App() {
     }
   };
 
-  const handlechange = e => {
-    setMessage(e.target.value);
-    if (e.target.value.trim() && nickName) {
-      sendBtnActive();
-    } else {
-      sendBtnDisabled();
-    }
-  };
-
-  const handlechangeSelect = e => {
-    setNickName(e.target.value);
-    if (message) {
-      sendBtnActive();
-    }
-  };
-
-  const sendBtnDisabled = () => {
-    document.querySelector('#sendBtn').disabled = true;
-  };
-  const sendBtnActive = () => {
-    document.querySelector('#sendBtn').disabled = false;
-  };
-
   const reset = () => {
     setMessage('');
     setNickName('');
-    sendBtnDisabled();
   };
 
   return (
@@ -85,50 +50,60 @@ function App() {
       <div className='container'>
         <div className='mainScreen'>
           <ul className='allMessage'>
-            {sentMsg.map(msg => (
-              <li key={nanoid()} className='item'>
-                <div className='avatarContainer'>
-                  <img
-                    src={avatars[msg.nickName]}
-                    alt='avatar'
-                    className='avatar'
-                  />
-                </div>
-                <p className='nickName'>{msg.nickName}</p>
-                <div className='messageContainer'>
-                  <span className='message'>{msg.message} </span>
-                  <span className='sendTime'>{msg.currentTime}</span>
-                </div>
-              </li>
-            ))}
+            {sentMsg.map(msg => {
+              const user = userProfile.find(
+                profile => profile.nickName === msg.nickName
+              );
+
+              // const avatarSrc = user ? user.avatar : '';
+              const avatarSrc = user
+                ? `${process.env.PUBLIC_URL}/${user.avatar}`
+                : '';
+              return (
+                <li key={nanoid()} className='item'>
+                  <div className='avatarContainer'>
+                    <img src={avatarSrc} alt='avatar' className='avatar' />
+                  </div>
+                  <p className='nickName'>{msg.nickName}</p>
+                  <div className='messageContainer'>
+                    <span className='message'>{msg.message} </span>
+                    <span className='sendTime'>{msg.currentTime}</span>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
         <form className='messageField' onSubmit={handleSubmit}>
           <input
             type='text'
             className='inputField'
-            onChange={handlechange}
+            onChange={e => setMessage(e.target.value)}
             value={message}
             onKeyDown={onKeyDown}
             placeholder='Enter your message'
           />
           <select
             value={nickName}
-            onChange={handlechangeSelect}
+            onChange={e => setNickName(e.target.value)}
             className='selectField'
             onKeyDown={onKeyDown}
           >
-            <option disabled hidden value=''>
+            <option hidden value=''>
               Choose nickName
             </option>
-            {nickNames.map(name => (
-              <option key={nanoid()} value={name}>
-                {name}
+            {userProfile.map(name => (
+              <option key={name.nickName} value={name.nickName}>
+                {name.nickName}
               </option>
             ))}
           </select>
 
-          <button id='sendBtn' type='submit' className='sendBtn' disabled>
+          <button
+            type='submit'
+            className='sendBtn'
+            disabled={!message || !nickName}
+          >
             Send
           </button>
         </form>
